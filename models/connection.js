e0c8f46f7b28479355694d921a32a8122908520a
johnealves <johne_alves@hotmail.com>
2021-08-14 19:19:34 -0300

    rename index

--- models/connection.js
+++ models/connection.js
@@ -1,12 +1,5 @@
 const mysql = require('mysql2/promise');
 
-// const connection = mysql.createPool({
-//   host: 'localhost',
-//   user: 'john',
-//   password: 'Password1@',
-//   database: 'osf898i7seo88lnz',
-// });
-
 const connection = mysql.createPool({
   host: 'kfgk8u2ogtoylkq9.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user: 'lfb5fx7xngfxmx2k',
